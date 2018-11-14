3d54bd29a4b7dda45152430b0028c137c5a767c9
Spencer <spencerapel@gmail.com>
2018-11-13 22:18:25 -0600

    Working on traking dots and dashes in the encode function

--- morse.cpp
+++ morse.cpp
@@ -26,8 +26,8 @@ void Morse::build_tree(const char *file_path)
 
 Morse::node *Morse::add_node(node *temp, string code, char c)
 {
-    if (temp == nullptr) //if temp is nullptr
-        temp = new node(); //create new node
+    if (temp == nullptr)    //if temp is nullptr
+        temp = new node();  //create new node
     if (code.length() == 0) //if there are no more . or _
     {
         temp->letter = c; //set letter to current node
@@ -35,9 +35,9 @@ Morse::node *Morse::add_node(node *temp, string code, char c)
     }
     else
     {
-        if (code[0] == dot) //if next char is a dot
-            temp->left = add_node(temp->left, code.substr(1), c); //go left and recursively call
-        else //if next char is a dash
+        if (code[0] == dot)                                         //if next char is a dot
+            temp->left = add_node(temp->left, code.substr(1), c);   //go left and recursively call
+        else                                                        //if next char is a dash
             temp->right = add_node(temp->right, code.substr(1), c); //go right and recursively call
     }
     return temp;
@@ -47,7 +47,9 @@ void Morse::encode(string message)
 {
     string code;
     cout << "Encoding: " << message << endl;
-    std::transform(message.begin(), message.end(), message.begin(), ::tolower); //make the string all lower case letters
+    std::transform(message.begin(), message.end(), message.begin(), tolower);
+    cout << message << endl;
+    //std::transform(message.begin(), message.end(), code.begin(), encode(root, message, code));
     encode(root, message, code);
     cout << "Code: " << code << endl;
 }
@@ -56,27 +58,31 @@ void Morse::encode(node *temp, string message, string &code)
 {
     cout << "currently at: " << temp->letter << endl;
 
-    if(message[0] == temp->letter)
+    if (message[0] == temp->letter)
     {
         cout << "FOUND IT!" << endl;
-        if(message.substr(1).length() != 0)
-            encode(root, message.substr(1), code);
+        if (message.substr(1).length() != 0)
+            encode(root, message.substr(1), code += " ");
+        return;
     }
-    else if(temp->left == nullptr || temp->right == nullptr)
+    
+    if (temp->left == nullptr && temp->right == nullptr)
     {
-        cout << "nullptr..." << endl;
-        if(temp->left == nullptr && temp->right == nullptr)
-            return;
-        else if(temp->left == nullptr)
-            encode(temp->right, message, code);
-        else if(temp->right == nullptr)
-            encode(temp->left, message, code);
+        code.pop_back();
+        return;
+    }
+    else if (temp->left == nullptr || temp->right == nullptr)
+    {
+        if (temp->left == nullptr)
+            encode(temp->right, message, code += dash);
+        else if (temp->right == nullptr)
+            encode(temp->left, message, code += dot);
     }
     else
     {
         cout << "rec call" << endl;
-        encode(temp->left, message, code);
-        encode(temp->right, message, code);
+        encode(temp->left, message, code += dot);
+        encode(temp->right, message, code += dash);
     }
 }
 
@@ -90,14 +96,14 @@ void Morse::decode(string code)
 
 void Morse::decode(node *temp, string code, string &message)
 {
-    if (code[0] == dot) //if char is a .
-        decode(temp->left, code.substr(1), message); //recursively call func with pointer to left and string code 1-length
-    else if (code[0] == dash) //if char is a _
-        decode(temp->right, code.substr(1), message); //recursively call func with pointer to right and string code 1-length
+    if (code[0] == dot)                                  //if char is a .
+        decode(temp->left, code.substr(1), message);     //recursively call func with pointer to left and string code 1-length
+    else if (code[0] == dash)                            //if char is a _
+        decode(temp->right, code.substr(1), message);    //recursively call func with pointer to right and string code 1-length
     else if (code[0] == delimiter || code.length() == 0) //if theres a space or at the end of the code
     {
-        message += temp->letter; //add letter to message
-        if(code.length() != 0) //if not at the end of the code        
+        message += temp->letter;                   //add letter to message
+        if (code.length() != 0)                    //if not at the end of the code
             decode(root, code.substr(1), message); //another recursive call using root (to start over from the top for the next char)
     }
 }

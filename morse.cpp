fcd583d079bb322caab57c25d9913d3243e624cc
Spencer <spencerapel@gmail.com>
2018-11-16 14:08:34 -0600

    Finished decode functions, added function for destruction of tree

--- morse.cpp
+++ morse.cpp
@@ -8,7 +8,17 @@ Morse::Morse(const char *file_path)
 
 Morse::~Morse()
 {
-    //delete[] root;
+    delete_nodes(root); //calls recursive function to delete the tree
+}
+
+void Morse::delete_nodes(node *temp)
+{
+    if(temp != nullptr)
+    {
+        delete_nodes(temp->left); //rec call for left
+        delete_nodes(temp->right); //rec call for right
+        delete temp; //delete the node
+    }
 }
 
 void Morse::build_tree(const char *file_path)
@@ -20,25 +30,26 @@ void Morse::build_tree(const char *file_path)
     for (int i = 0; i < alphabet_size; i++) //loop through the file
     {
         file_in >> morse_file;
-        root = add_node(root, morse_file.substr(1), morse_file[0]); //create a node and add to root
+        root = add_node(root, morse_file.substr(1), morse_file[0], morse_file.substr(1)); //create a node and add to root
     }
 }
 
-Morse::node *Morse::add_node(node *temp, string code, char c)
+Morse::node *Morse::add_node(node *temp, string code, char c, string code2)
 {
-    if (temp == nullptr)    //if temp is nullptr
-        temp = new node();  //create new node
+    if (temp == nullptr) //if temp is nullptr
+        temp = new node(); //create new node
     if (code.length() == 0) //if there are no more . or _
     {
         temp->letter = c; //set letter to current node
+        temp->code = code2; //set code2 (morse code translation) into current node code
         return temp;
     }
     else
     {
-        if (code[0] == dot)                                         //if next char is a dot
-            temp->left = add_node(temp->left, code.substr(1), c);   //go left and recursively call
-        else                                                        //if next char is a dash
-            temp->right = add_node(temp->right, code.substr(1), c); //go right and recursively call
+        if (code[0] == dot) //if next char is a dot
+            temp->left = add_node(temp->left, code.substr(1), c, code2); //go left and recursively call
+        else //if next char is a dash
+            temp->right = add_node(temp->right, code.substr(1), c, code2); //go right and recursively call
     }
     return temp;
 }
@@ -47,42 +58,28 @@ void Morse::encode(string message)
 {
     string code;
     cout << "Encoding: " << message << endl;
-    std::transform(message.begin(), message.end(), message.begin(), tolower);
-    cout << message << endl;
-    //std::transform(message.begin(), message.end(), code.begin(), encode(root, message, code));
-    encode(root, message, code);
+    std::transform(message.begin(), message.end(), message.begin(), tolower); //make all characters in string lowercase
+    encode(root, message, code); //call recursive function to encode the message
     cout << "Code: " << code << endl;
 }
 
 void Morse::encode(node *temp, string message, string &code)
 {
-    cout << "currently at: " << temp->letter << endl;
-
-    if (message[0] == temp->letter)
+    if (message[0] == temp->letter) //if the letter is found
     {
-        cout << "FOUND IT!" << endl;
-        if (message.substr(1).length() != 0)
-            encode(root, message.substr(1), code += " ");
+        code += temp->code; //set string code to the char's code
+        if (message.substr(1).length() != 0) //if not at the last letter
+            encode(root, message.substr(1), code += " "); //recursive call where the message loses the first char, and a space is added to code
         return;
     }
-    
-    if (temp->left == nullptr && temp->right == nullptr)
-    {
-        code.pop_back();
+    if (temp->left == nullptr && temp->right == nullptr) //if left and right are nullptr
         return;
-    }
-    else if (temp->left == nullptr || temp->right == nullptr)
+    else if (temp->right == nullptr) //if right is nullptr (never have to check for left bc of the way the tree is built in morse code)
+        encode(temp->left, message, code); //recursive call with to the left child
+    else //if node is full
     {
-        if (temp->left == nullptr)
-            encode(temp->right, message, code += dash);
-        else if (temp->right == nullptr)
-            encode(temp->left, message, code += dot);
-    }
-    else
-    {
-        cout << "rec call" << endl;
-        encode(temp->left, message, code += dot);
-        encode(temp->right, message, code += dash);
+        encode(temp->left, message, code); //rec call left
+        encode(temp->right, message, code); //rec call right
     }
 }
 
@@ -96,14 +93,14 @@ void Morse::decode(string code)
 
 void Morse::decode(node *temp, string code, string &message)
 {
-    if (code[0] == dot)                                  //if char is a .
-        decode(temp->left, code.substr(1), message);     //recursively call func with pointer to left and string code 1-length
-    else if (code[0] == dash)                            //if char is a _
-        decode(temp->right, code.substr(1), message);    //recursively call func with pointer to right and string code 1-length
-    else if (code[0] == delimiter || code.length() == 0) //if theres a space or at the end of the code
+    if (code[0] == dot) //if char is a .
+        decode(temp->left, code.substr(1), message); //recursively call func with pointer to left and string code 1-length
+    else if (code[0] == dash) //if char is a _
+        decode(temp->right, code.substr(1), message); //recursively call func with pointer to right and string code 1-length
+    else if (code[0] == space || code.length() == 0) //if theres a space or at the end of the code
     {
-        message += temp->letter;                   //add letter to message
-        if (code.length() != 0)                    //if not at the end of the code
+        message += temp->letter; //add letter to message
+        if (code.length() != 0) //if not at the end of the code
             decode(root, code.substr(1), message); //another recursive call using root (to start over from the top for the next char)
     }
 }
--- morse.h
+++ morse.h
@@ -21,18 +21,18 @@ class Morse
         node *right;
 
         node(){letter = ' '; code = " "; left = nullptr; right = nullptr;} //node constructor
-    };
-    node *root;
+    } *root;
 
     const char dot = '.'; //go left
     const char dash = '_'; //go right
-    const char delimiter = ' '; //space delimited for decoding string
+    const char space = ' '; //space delimited for decoding string
     const int alphabet_size = 26;
 
     void build_tree(const char *file_path); //opens file and builds tree by calling add_node()
-    node *add_node(node *temp, string code, char c); //adds a node to the tree is respective location
+    node *add_node(node *temp, string code, char c, string code2); //adds a node to the tree is respective location
     void encode(node *temp, string message, string &code); //function to encode regular text
     void decode(node *temp, string code, string &message); //function to decode morse text
+    void delete_nodes(node *temp); //to delete the tree at the end of the program
 
   public:
     Morse(const char *file_path);

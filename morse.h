b97ba576a7800950380c494508884414a240e431
SpencerApel <spencerapel@gmail.com>
2018-11-26 13:08:36 -0600

    Fixed comments, and autoformat

--- morse.h
+++ morse.h
@@ -3,9 +3,9 @@
 
 #include <iostream>
 #include <string>
-#include <fstream>   //for file input
+#include <fstream> //for file input
 #include <algorithm> //used for std::transform in encode(string)
-#include <ctype.h>   //to check if a char is part of the alphabet isalpha()
+#include <ctype.h> //to check if a char is part of the alphabet isalpha()
 
 using std::cout;
 using std::endl;
@@ -13,39 +13,33 @@ using std::string;
 
 class Morse
 {
-private:
-  struct node
-  {
-    char letter;
-    string code;
-    node *left;
-    node *right;
-
-    node()
+  private:
+    struct node
     {
-      letter = ' ';
-      code = " ";
-      left = nullptr;
-      right = nullptr;
-    } //node constructor
-  } * root;
+        char letter;
+        string code;
+        node *left;
+        node *right;
 
-  const char DOT = '.';   //go left
-  const char DASH = '_';  //go right
-  const char SPACE = ' '; //space delimited for decoding string
-  const int ALPHABET_SIZE = 26;
+        node(){letter = ' '; code = " "; left = nullptr; right = nullptr;} //node constructor
+    } *root;
 
-  void build_tree(const char *file_path);                                   //opens file and builds tree by calling add_node()
-  node *add_node(node *temp, string code, char c, const string morse_code); //adds a node to the tree is respective location
-  void encode(node *temp, string &message, string &code);                   //function to encode regular text
-  void decode(node *temp, string code, string &message);                    //function to decode morse text
-  void delete_nodes(node *temp);                                            //to delete the tree at the end of the program
+    const char DOT = '.'; //go left
+    const char DASH = '_'; //go right
+    const char SPACE = ' '; //space delimited for decoding string
+    const int ALPHABET_SIZE = 26;
 
-public:
-  Morse(const char *file_path);
-  ~Morse();
-  void encode(string code); //wrapper function
-  void decode(string code); //wrapper function
+    void build_tree(const char *file_path); //opens file and builds tree by calling add_node()
+    node *add_node(node *temp, string code, char c, const string morse_code); //adds a node to the tree is respective location
+    void encode(node *temp, string &message, string &code); //function to encode regular text
+    void decode(node *temp, string code, string &message); //function to decode morse text
+    void delete_nodes(node *temp); //to delete the tree at the end of the program
+
+  public:
+    Morse(const char *file_path);
+    ~Morse();
+    void encode(string code); //wrapper function
+    void decode(string code); //wrapper function
 };
 
 #endif
